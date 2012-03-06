4bea938cbe2eabf61911a652dc334c78a7fc6903
Iztok Jeras <iztok.jeras@gmail.com>
2012-03-06 14:42:30 +0100

    added extra literal values

--- test_regress/t/t_array_packed_literals.v
+++ test_regress/t/t_array_packed_literals.v
@@ -14,7 +14,7 @@ module t (/*AUTOARG*/
   localparam WA = 4;  // address dimension size
   localparam WB = 4;  // bit     dimension size
 
-  localparam NO = 7;  // number of access events
+  localparam NO = 10;  // number of access events
 
   // 2D packed arrays
   logic [WA-1:0] [WB-1:0] array_bg;  // big endian array
@@ -43,24 +43,34 @@ module t (/*AUTOARG*/
     else if (cnt[30:2]==3)  array_bg <=  {WA{ {WB{1'bx}} }};
     else if (cnt[30:2]==4)  array_bg <=  {WA{ {WB{1'bx}} }};
     else if (cnt[30:2]==5)  array_bg <=  {WA{ {WB{1'bx}} }};
+    else if (cnt[30:2]==6)  array_bg <=  {WA{ {WB{1'bx}} }};
+    else if (cnt[30:2]==7)  array_bg <=  {WA{ {WB{1'bx}} }};
+    else if (cnt[30:2]==8)  array_bg <=  {WA{ {WB{1'bx}} }};
+    else if (cnt[30:2]==9)  array_bg <=  {WA{ {WB{1'bx}} }};
   end else if (cnt[1:0]==2'd1) begin
     // write data into whole or part of the array using literals
     if      (cnt[30:2]==0)  begin end
-    else if (cnt[30:2]==1)  array_bg                            = '{ 3 ,2 ,1, 0 };
-    else if (cnt[30:2]==2)  array_bg                            = '{WA  {          {WB/2  {2'b10}}  }};
-    else if (cnt[30:2]==3)  array_bg                            = '{WA  { {3'b101, {WB/2-1{2'b10}}} }};
-    else if (cnt[30:2]==4)  array_bg                            = '{WA  {          {WB/2-1{2'b10}}  }};
-    else if (cnt[30:2]==5)  array_bg [WA/2-1:0   ]              = '{WA/2{          {WB/2  {2'b10}}  }};
-    else if (cnt[30:2]==6)  array_bg [WA  -1:WA/2]              = '{WA/2{          {WB/2  {2'b01}}  }};
+    else if (cnt[30:2]==1)  array_bg               = '{ 3 ,2 ,1, 0 };
+    else if (cnt[30:2]==2)  array_bg               = '{0:4, 1:5, 2:6, 3:7};
+    else if (cnt[30:2]==3)  array_bg               = '{default:13};
+    else if (cnt[30:2]==4)  array_bg               = '{2:15, default:13};
+    else if (cnt[30:2]==5)  array_bg               = '{WA  {          {WB/2  {2'b10}}  }};
+    else if (cnt[30:2]==6)  array_bg               = '{WA  { {3'b101, {WB/2-1{2'b10}}} }};
+    else if (cnt[30:2]==7)  array_bg               = '{WA  {          {WB/2-1{2'b10}}  }};
+    else if (cnt[30:2]==8)  array_bg [WA/2-1:0   ] = '{WA/2{          {WB/2  {2'b10}}  }};
+    else if (cnt[30:2]==9)  array_bg [WA  -1:WA/2] = '{WA/2{          {WB/2  {2'b01}}  }};
   end else if (cnt[1:0]==2'd2) begin
     // chack array agains expected value
     if      (cnt[30:2]==0)  begin if (array_bg !== 16'bxxxxxxxxxxxxxxxx) begin $display("%b", array_bg); $stop(); end end
     else if (cnt[30:2]==1)  begin if (array_bg !== 16'b0011001000010000) begin $display("%b", array_bg); $stop(); end end
-    else if (cnt[30:2]==2)  begin if (array_bg !== 16'b1010101010101010) begin $display("%b", array_bg); $stop(); end end
-    else if (cnt[30:2]==3)  begin if (array_bg !== 16'b0110011001100110) begin $display("%b", array_bg); $stop(); end end
-    else if (cnt[30:2]==4)  begin if (array_bg !== 16'b0010001000100010) begin $display("%b", array_bg); $stop(); end end
-    else if (cnt[30:2]==5)  begin if (array_bg !== 16'b10101010xxxxxxxx) begin $display("%b", array_bg); $stop(); end end
-    else if (cnt[30:2]==6)  begin if (array_bg !== 16'bxxxxxxxx10101010) begin $display("%b", array_bg); $stop(); end end
+    else if (cnt[30:2]==2)  begin if (array_bg !== 16'b0111011001010100) begin $display("%b", array_bg); $stop(); end end
+    else if (cnt[30:2]==3)  begin if (array_bg !== 16'b1101110111011101) begin $display("%b", array_bg); $stop(); end end
+    else if (cnt[30:2]==4)  begin if (array_bg !== 16'b1101111111011101) begin $display("%b", array_bg); $stop(); end end
+    else if (cnt[30:2]==5)  begin if (array_bg !== 16'b1010101010101010) begin $display("%b", array_bg); $stop(); end end
+    else if (cnt[30:2]==6)  begin if (array_bg !== 16'b0110011001100110) begin $display("%b", array_bg); $stop(); end end
+    else if (cnt[30:2]==7)  begin if (array_bg !== 16'b0010001000100010) begin $display("%b", array_bg); $stop(); end end
+    else if (cnt[30:2]==8)  begin if (array_bg !== 16'b10101010xxxxxxxx) begin $display("%b", array_bg); $stop(); end end
+    else if (cnt[30:2]==9)  begin if (array_bg !== 16'bxxxxxxxx10101010) begin $display("%b", array_bg); $stop(); end end
   end
 
 endmodule
