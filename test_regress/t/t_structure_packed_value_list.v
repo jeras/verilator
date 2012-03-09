d84741f7c87c097877d424ec8299d7c1fe6df81e
Iztok Jeras <iztok.jeras@gmail.com>
2012-03-09 18:49:22 +0100

    added tests with variables inside the value list

--- test_regress/t/t_structure_packed_value_list.v
+++ test_regress/t/t_structure_packed_value_list.v
@@ -10,7 +10,7 @@ module t (/*AUTOARG*/
 
    input clk;
 
-   localparam NO = 6;  // number of access events
+   localparam NO = 7;  // number of access events
 
    // packed structures
    struct packed {
@@ -55,6 +55,7 @@ module t (/*AUTOARG*/
       else if (cnt[30:2]==3)  struct_bg <= {WS{1'bx}};
       else if (cnt[30:2]==4)  struct_bg <= {WS{1'bx}};
       else if (cnt[30:2]==5)  struct_bg <= {WS{1'bx}};
+      else if (cnt[30:2]==6)  struct_bg <= {WS{1'bx}};
    end else if (cnt[1:0]==2'd1) begin
       // write data into whole or part of the array using literals
       if      (cnt[30:2]==0)  begin end
@@ -63,6 +64,7 @@ module t (/*AUTOARG*/
       else if (cnt[30:2]==3)  struct_bg <= '{e3:6, e2:4, e1:2, e0:0};
       else if (cnt[30:2]==4)  struct_bg <= '{default:13};
       else if (cnt[30:2]==5)  struct_bg <= '{e2:8'haa, default:1};
+      else if (cnt[30:2]==6)  struct_bg <= '{cnt+0 ,cnt+1 , cnt+2, cnt+3};
    end else if (cnt[1:0]==2'd2) begin
       // chack array agains expected value
       if      (cnt[30:2]==0)  begin if (struct_bg !== 15'bx_xx_xxxx_xxxxxxxx) begin $display("%b", struct_bg); $stop(); end end
@@ -71,6 +73,7 @@ module t (/*AUTOARG*/
       else if (cnt[30:2]==3)  begin if (struct_bg !== 15'b0_10_0100_00000110) begin $display("%b", struct_bg); $stop(); end end
       else if (cnt[30:2]==4)  begin if (struct_bg !== 15'b1_01_1101_00001101) begin $display("%b", struct_bg); $stop(); end end
       else if (cnt[30:2]==5)  begin if (struct_bg !== 15'b1_01_1010_00000001) begin $display("%b", struct_bg); $stop(); end end
+      else if (cnt[30:2]==6)  begin if (struct_bg !== 15'b1_10_1011_00011100) begin $display("%b", struct_bg); $stop(); end end
    end
 
    // little endian
@@ -83,6 +86,7 @@ module t (/*AUTOARG*/
       else if (cnt[30:2]==3)  struct_lt <= {WS{1'bx}};
       else if (cnt[30:2]==4)  struct_lt <= {WS{1'bx}};
       else if (cnt[30:2]==5)  struct_lt <= {WS{1'bx}};
+      else if (cnt[30:2]==6)  struct_lt <= {WS{1'bx}};
    end else if (cnt[1:0]==2'd1) begin
       // write data into whole or part of the array using literals
       if      (cnt[30:2]==0)  begin end
@@ -91,6 +95,7 @@ module t (/*AUTOARG*/
       else if (cnt[30:2]==3)  struct_lt <= '{e3:6, e2:4, e1:2, e0:0};
       else if (cnt[30:2]==4)  struct_lt <= '{default:13};
       else if (cnt[30:2]==5)  struct_lt <= '{e2:8'haa, default:1};
+      else if (cnt[30:2]==6)  struct_lt <= '{cnt+0 ,cnt+1 , cnt+2, cnt+3};
    end else if (cnt[1:0]==2'd2) begin
       // chack array agains expected value
       if      (cnt[30:2]==0)  begin if (struct_lt !== 15'bx_xx_xxxx_xxxxxxxx) begin $display("%b", struct_lt); $stop(); end end
@@ -99,6 +104,7 @@ module t (/*AUTOARG*/
       else if (cnt[30:2]==3)  begin if (struct_lt !== 15'b0_10_0100_00000110) begin $display("%b", struct_lt); $stop(); end end
       else if (cnt[30:2]==4)  begin if (struct_lt !== 15'b1_01_1101_00001101) begin $display("%b", struct_lt); $stop(); end end
       else if (cnt[30:2]==5)  begin if (struct_lt !== 15'b1_01_1010_00000001) begin $display("%b", struct_lt); $stop(); end end
+      else if (cnt[30:2]==6)  begin if (struct_lt !== 15'b1_10_1011_00011100) begin $display("%b", struct_lt); $stop(); end end
    end
 
 endmodule
